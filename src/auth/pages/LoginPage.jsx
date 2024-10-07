3ecb35063440f99cbcd271900023368fcd79d4f2
Rodrigo <rodrigofarii@gmail.com>
2024-10-07 15:53:24 -0600

    Add NavBar layout

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -26,18 +26,18 @@ export const LoginPage = () => {
             </Grid2>
           </Grid2>
           <Grid2 container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
-              <Grid2 size={{xs:12, sm: 6}}>
-                <Button variant='contained' fullWidth>
-                  Login
-                </Button>
-              </Grid2>
-              <Grid2 size={{xs:12, sm: 6}}>
-                <Button variant='contained' fullWidth>
-                  <Google/>
-                  <Typography sx={{ml:1}}>Google</Typography>
-                </Button>
-              </Grid2>
+            <Grid2 size={{xs:12, sm: 6}}>
+              <Button variant='contained' fullWidth>
+                Login
+              </Button>
             </Grid2>
+            <Grid2 size={{xs:12, sm: 6}}>
+              <Button variant='contained' fullWidth>
+                <Google/>
+                <Typography sx={{ml:1}}>Google</Typography>
+              </Button>
+            </Grid2>
+          </Grid2>
           <Grid2 container direction='row' justifyContent='end'>
             <Link component={RouterLink} color="inherit" to="/auth/register">
               Crear una cuenta
