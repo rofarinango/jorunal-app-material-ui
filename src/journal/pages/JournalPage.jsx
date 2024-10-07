165ead8c597fd165dfac57414d4f5f1170476acb
Rodrigo <rodrigofarii@gmail.com>
2024-10-07 15:34:54 -0600

    Add JournalLayout for journal page

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,14 +1,13 @@
 import { MailOutline } from "@mui/icons-material"
 import { Typography } from "@mui/material"
+import { JournalLayout } from "../layout/JournalLayout"
 
 export const JournalPage = () => {
   return (
-    <>
-      <Typography 
-      variant='h1'
-      >
-        JournalPage
-      </Typography>
-    </>
+      <JournalLayout>
+        <Typography>Laborum excepteur tempor elit do nostrud laboris sint proident id. Quis ut consequat veniam pariatur reprehenderit nostrud deserunt dolore. Id non reprehenderit duis commodo esse exercitation duis anim amet ullamco. Magna velit anim elit nisi pariatur id. Laborum anim veniam amet eu culpa ea duis consectetur pariatur eu cillum duis commodo proident. Aliqua amet dolor id reprehenderit irure adipisicing laborum do aute.</Typography>
+        {/* NothingSelected */}
+        {/* NoteView */}
+      </JournalLayout>
   )
 }
