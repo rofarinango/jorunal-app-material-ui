6cc26ace4cd29fc40085db9c9c63d5d54238a28e
Rodrigo <rodrigofarii@gmail.com>
2024-10-08 15:57:55 -0600

    Add NoteView component

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,15 @@
 import { MailOutline } from "@mui/icons-material"
-import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
 
 export const JournalPage = () => {
   return (
       <JournalLayout>
         {/* <Typography>Laborum excepteur tempor elit do nostrud laboris sint proident id. Quis ut consequat veniam pariatur reprehenderit nostrud deserunt dolore. Id non reprehenderit duis commodo esse exercitation duis anim amet ullamco. Magna velit anim elit nisi pariatur id. Laborum anim veniam amet eu culpa ea duis consectetur pariatur eu cillum duis commodo proident. Aliqua amet dolor id reprehenderit irure adipisicing laborum do aute.</Typography> */}
         {/* NothingSelected */}
-        <NothingSelectedView/>
+        {/* <NothingSelectedView/> */}
         {/* NoteView */}
+        <NoteView/>
       </JournalLayout>
   )
 }
