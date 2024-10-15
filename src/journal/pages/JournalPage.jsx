07aa6279bb3a677adb8a1c9cead11b535aec6fd7
Rodrigo <rodrigofarii@gmail.com>
2024-10-15 13:49:54 -0600

    Add New Note Button on JournalPage

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,15 +1,30 @@
-import { MailOutline } from "@mui/icons-material"
+import { AddOutlined, MailOutline } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
+import { IconButton } from "@mui/material"
+
 
 export const JournalPage = () => {
   return (
       <JournalLayout>
         {/* <Typography>Laborum excepteur tempor elit do nostrud laboris sint proident id. Quis ut consequat veniam pariatur reprehenderit nostrud deserunt dolore. Id non reprehenderit duis commodo esse exercitation duis anim amet ullamco. Magna velit anim elit nisi pariatur id. Laborum anim veniam amet eu culpa ea duis consectetur pariatur eu cillum duis commodo proident. Aliqua amet dolor id reprehenderit irure adipisicing laborum do aute.</Typography> */}
         {/* NothingSelected */}
-        {/* <NothingSelectedView/> */}
+        <NothingSelectedView/>
         {/* NoteView */}
-        <NoteView/>
+
+        <IconButton
+          size="large"
+          sx={{
+            color: 'white',
+            backgroundColor: 'error.main',
+            ':hover': {backgroundColor: 'error.main', opacity: 0.9},
+            position: 'fixed',
+            right: 50,
+            bottom: 50
+          }}
+        >
+          <AddOutlined sx={{fontSize: 30}}/>
+        </IconButton>
       </JournalLayout>
   )
 }
