bae9b34720e99d13cc4e0c10e0d07383369a7520
Rodrigo <rodrigofarii@gmail.com>
2024-11-07 13:33:48 -0600

    Added redux initial config

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import { createRoot } from 'react-dom/client'
 import { JournalApp } from './JournalApp.jsx'
 import './styles.css'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { store } from './store'
+
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter>
-      <JournalApp />
-    </BrowserRouter>
+    <Provider store={ store }>
+      <BrowserRouter>
+        <JournalApp />
+      </BrowserRouter>
+    </Provider>
+    
   </StrictMode>,
 )
