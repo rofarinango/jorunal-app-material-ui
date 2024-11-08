f2874b9ae73804fc50b32e590af512cb1c576c8c
Rodrigo <rodrigofarii@gmail.com>
2024-11-07 18:15:35 -0600

    Added register new user form handle on submit

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -2,11 +2,28 @@ import { Link as RouterLink} from 'react-router-dom'
 import { Google } from "@mui/icons-material"
 import { Grid2, TextField, Typography, Button, Link } from "@mui/material"
 import { AuthLayout } from '../layout/AuthLayout'
+import { useForm } from '../../hooks'
+
+const formData = {
+  email: 'fernando@google.com',
+  password: '123456',
+  displayName: 'Fernando Herrera'
+}
+
 
 export const RegisterPage = () => {
+
+  const { displayName, email, password, onInputChange, formState } = useForm(formData);
+   
+  const onSubmit = ( event ) => {
+    event.preventDefault();
+    console.log(formState);
+    
+  }
+
   return (
     <AuthLayout title='Crear Cuenta'>
-      <form>
+      <form onSubmit={onSubmit}>
           <Grid2 container>
             <Grid2 size={{xs: 12}} sx={{mt: 2}}>
                 <TextField
@@ -14,6 +31,9 @@ export const RegisterPage = () => {
                   type="text" 
                   placeholder="John Doe"
                   fullWidth
+                  name='displayName'
+                  value={displayName}
+                  onChange={ onInputChange }
                 />
             </Grid2>
             <Grid2 size={{xs: 12}} sx={{mt: 2}}>
@@ -22,6 +42,9 @@ export const RegisterPage = () => {
                 type="email" 
                 placeholder="john@doe.com"
                 fullWidth
+                name='email'
+                value={email}
+                onChange={ onInputChange }
               />
             </Grid2>
             <Grid2 size={{xs: 12}} sx={{mt: 2}}>
@@ -30,12 +53,15 @@ export const RegisterPage = () => {
                 type="password" 
                 placeholder="Contrasena"
                 fullWidth
+                name='password'
+                value={password}
+                onChange={ onInputChange }
               />
             </Grid2>
           </Grid2>
           <Grid2 container spacing={ 2 } sx={{ mb: 2, mt: 1 }}>
             <Grid2 size={{xs:12}}>
-              <Button variant='contained' fullWidth>
+              <Button type='submit' variant='contained' fullWidth>
                 Crear Cuenta
               </Button>
             </Grid2>
