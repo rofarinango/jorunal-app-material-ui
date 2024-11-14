648810e861bcde1fa32513e9787f2349f59b9207
Rodrigo <rodrigofarii@gmail.com>
2024-11-14 11:24:05 -0600

    Add startNewNote with uid user auth

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -2,9 +2,18 @@ import { AddOutlined, MailOutline } from "@mui/icons-material"
 import { JournalLayout } from "../layout/JournalLayout"
 import { NoteView, NothingSelectedView } from "../views"
 import { IconButton } from "@mui/material"
+import { startNewNote } from "../../store/journal/thunks"
+import { useDispatch } from "react-redux"
 
 
 export const JournalPage = () => {
+
+
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  }
   return (
       <JournalLayout>
         {/* <Typography>Laborum excepteur tempor elit do nostrud laboris sint proident id. Quis ut consequat veniam pariatur reprehenderit nostrud deserunt dolore. Id non reprehenderit duis commodo esse exercitation duis anim amet ullamco. Magna velit anim elit nisi pariatur id. Laborum anim veniam amet eu culpa ea duis consectetur pariatur eu cillum duis commodo proident. Aliqua amet dolor id reprehenderit irure adipisicing laborum do aute.</Typography> */}
@@ -13,6 +22,7 @@ export const JournalPage = () => {
         {/* NoteView */}
 
         <IconButton
+        onClick={onClickNewNote}
           size="large"
           sx={{
             color: 'white',
