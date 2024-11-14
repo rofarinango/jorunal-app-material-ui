ee7496d16b3078b74c86e7f3477fb6d1faad6e5a
Rodrigo <rodrigofarii@gmail.com>
2024-11-14 16:38:01 -0600

    Load notes form firebase

--- src/store/journal/thunks.js
+++ src/store/journal/thunks.js
@@ -1,6 +1,7 @@
 import { collection, doc, setDoc } from "firebase/firestore/lite";
 import { FirebaseDB } from "../../firebase/config";
-import { addNewEmptyNote, savingNewNote, setActiveNote } from "./journalSlice";
+import { addNewEmptyNote, savingNewNote, setActiveNote, setNotes } from "./journalSlice";
+import { loadNotes } from "../../helpers/";
 
 export const startNewNote = () => {
     return async( dispatch, getState ) => {
@@ -25,6 +26,17 @@ export const startNewNote = () => {
         dispatch( addNewEmptyNote( newNote ) );
         
         dispatch( setActiveNote( newNote ) );
+
+    }
+}
+
+export const startLoadingNotes = () => {
+    return async ( dispatch, getState ) => {
+        const { uid } = getState().auth;
+        if (!uid) throw new Error('El UID del usuario no existe');
+
+        const notes = await loadNotes(uid);
+        dispatch(setNotes(notes));
 
     }
 }
