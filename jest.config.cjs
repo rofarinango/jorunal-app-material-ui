572afa0455895bb314ea60ba6e0419a9f0cfd12f
Rodrigo <rodrigofarii@gmail.com>
2024-11-21 14:48:50 -0600

    Add test on auth thunks

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,5 @@
 module.exports = {
-    moduleNameMapper: {
-        "^@/(.*)$": "<rootDir>/src/$1",
-    },
     testEnvironment: 'jest-environment-jsdom',
     setupFiles: ['./jest.setup.js'],
-    transform: {
-        "^.+\\.[t|j]sx?$": "babel-jest",
-    },
+    transformIgnorePatterns: [],
 }
